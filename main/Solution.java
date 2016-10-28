51f3195ebeb2e475438b59a1c8ebd1f948ee767e
mohanmb91 <mohankumarmbuniv@gmail.com>
2016-10-28 02:39:45 -0700

    changes

--- main/Solution.java
+++ main/Solution.java
@@ -5,6 +5,55 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
+
+public class Solution {
+public static void main(String[] args) {
+	NTree tree = new NTree();
+	TreeNode root = new TreeNode("start", null);
+	tree.setRoot(root);
+	String input = "dir1\n dir11\n  dir111\n   mohan.jpeg\ndir2\ndir3\n dir31\n  naveen.jpeg\n guru.jpeg\ndir4";
+	System.out.println(input);
+	HashMap<Integer, String> levelTrack = new HashMap<>();
+	String[] dirs = input.split("\n");
+	String current = null;
+	int currentLevel = Integer.MAX_VALUE,maxPathLength = 0;
+	String maxPath = "";
+	for(String eachdir: dirs){
+		
+		String[] spaces = eachdir.split(" ");
+		currentLevel = spaces.length -1;
+		current = eachdir;
+		current = current.replaceAll(" ", "");
+		if(!current.contains(".")){
+			levelTrack.put(currentLevel, current);
+		}
+		else{
+			String path = "";
+			int tempPathLength = 0;
+			for(int i =0; i <= currentLevel; i ++){
+				if(i == currentLevel){
+					tempPathLength = path.length();
+					path += current;
+					break;
+				}
+				path += levelTrack.get(i) + "/";
+			}
+			
+			if(tempPathLength > maxPathLength){
+				maxPathLength = tempPathLength;
+				maxPath = path;
+			}
+		}
+
+	}
+	System.out.println(maxPath+"   =====>    "+maxPathLength );
+	System.out.println("end");
+}
+}
+
+
+
+
 class TreeNode{
 	String directoryName;
 	List<TreeNode> childrens;
@@ -57,64 +106,3 @@ class NTree{
 		return null;
 	}
 }
-public class Solution {
-public static void main(String[] args) {
-	NTree tree = new NTree();
-	TreeNode root = new TreeNode("start", null);
-	tree.setRoot(root);
-	String input = "dir1\n dir11\n  dir111\n   mohan.jpeg\ndir2\ndir3\n dir31\n  naveen.jpeg\n guru.jpeg\ndir4";
-	System.out.println(input);
-	HashMap<Integer, String> levelTrack = new HashMap<>();
-	String[] dirs = input.split("\n");
-	String current = null;
-	int currentLevel = Integer.MAX_VALUE,maxPathLength = 0;
-	String maxPath = "";
-	for(String eachdir: dirs){
-		
-		String[] spaces = eachdir.split(" ");
-		currentLevel = spaces.length -1;
-		current = eachdir;
-		current = current.replaceAll(" ", "");
-		if(!current.contains(".")){
-			levelTrack.put(currentLevel, current);
-		}
-		else{
-			String path = "";
-			for(int i =0; i <= currentLevel; i ++){
-				if(i == currentLevel){
-					path += current;
-					break;
-				}
-				path += levelTrack.get(i) + "/";
-			}
-			int tempPathLength = path.length();
-			if(tempPathLength > maxPathLength){
-				maxPathLength = tempPathLength;
-				maxPath = path;
-			}
-		}
-
-	}
-	System.out.println(maxPath+"   =====>    "+maxPathLength );
-	System.out.println("end");
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-//if(currentLevel == 0){
-//tree.addNode(tree.root, new TreeNode(current, tree.root));
-//}else{
-//if(levelTrack.containsKey(currentLevel -1)){
-//tree.addNode(tree.searchNode(levelTrack.get(currentLevel -1)), new TreeNode(current, tree.searchNode(levelTrack.get(currentLevel -1))) );
-//}
-//}
