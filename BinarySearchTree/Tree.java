aec992b6b5beb0f003b02aa24c925178992f2fbf
mohanmb91 <mohankumarmbuniv@gmail.com>
2016-11-07 17:07:48 -0800

    clean

--- BinarySearchTree/Tree.java
+++
@@ -1,8 +0,0 @@
-package BinarySearchTree;
-
-public interface Tree<D extends Comparable> {
-	public boolean isEmpty();
-	public int cardinality();
-	public boolean member(D elt);
-	public NonEmptyBST<D> add(D elt);
-}
